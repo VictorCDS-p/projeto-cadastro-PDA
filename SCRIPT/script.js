2bbfdb6e415e5f604e8d30ebf7fc741037031394
VictorCDS-p <Victor90774@outlook.com>
2024-03-03 01:26:33 -0300

    add sistema de verificacao

--- SCRIPT/script.js
+++ SCRIPT/script.js
@@ -5,15 +5,17 @@ let idadeAluno = document.getElementById("idadeAluno");
 let nota = document.getElementById("nota");
 let enviarButton = document.getElementById("enviar");
 let cadastrarNovoAluno = document.getElementById("cadastrarNovoAluno")
-let aluno = document.getElementsByClassName("aluno")[0]
+let formAluno = document.getElementsByClassName("aluno")[0]
 
 cadastrarNovoAluno.addEventListener("click", function() {
-    aluno.style.display = "block"
+    formAluno.style.display = "block"
+    cadastrarNovoAluno.style.display = "none"
 });
 
 
 enviarButton.addEventListener("click", function () {
-    class PegandoAlunos {
+
+        class PegandoAlunos {
         constructor(nome, sobrenome, idade, nota) {
             this.nome = nome;
             this.sobrenome = sobrenome;
@@ -31,27 +33,41 @@ enviarButton.addEventListener("click", function () {
 
 
 function createTable(aluno) {
-    let tabela = document.querySelector("table");
-    let linha = document.createElement("tr");
+    let divErro = document.getElementById("erro");
+    divErro.innerHTML = "";
+    if(nomeAluno.value.length > 3 && sobrenomeAluno.value.length > 3 && idadeAluno.value <= 18 && nota.value <= 10 ){
+        let tabela = document.querySelector("table");
+        let linha = document.createElement("tr");
+    
+        let caixaNome = document.createElement("td");
+        caixaNome.textContent = aluno.nome + " " + aluno.sobrenome;
+        linha.appendChild(caixaNome);
+    
+        let caixaIdade = document.createElement("td");
+        caixaIdade.textContent = aluno.idade;
+        linha.appendChild(caixaIdade);
+    
+        let caixaNota = document.createElement("td");
+        caixaNota.textContent = aluno.nota;
+        linha.appendChild(caixaNota);
+    
+        tabela.appendChild(linha);
+    
+        linha.setAttribute("class", "linha")
+        caixaNome.setAttribute("class", "caixa1")
+        caixaIdade.setAttribute("class", "caixa2")
+        caixaNota.setAttribute("class", "caixa3")
+    }else{
+        let divErro = document.getElementById("erro")
+        let pErro = document.createElement("p");
+        pErro.innerHTML = "Erro, digite os valores corretos";
+        divErro.appendChild(pErro);
+    }
 
-    let caixaNome = document.createElement("td");
-    caixaNome.textContent = aluno.nome + " " + aluno.sobrenome;
-    linha.appendChild(caixaNome);
+    cadastrarNovoAluno.style.display = "block"
+    formAluno.style.display = "none"
 
-    let caixaIdade = document.createElement("td");
-    caixaIdade.textContent = aluno.idade;
-    linha.appendChild(caixaIdade);
 
-    let caixaNota = document.createElement("td");
-    caixaNota.textContent = aluno.nota;
-    linha.appendChild(caixaNota);
-
-    tabela.appendChild(linha);
-
-    linha.setAttribute("class", "linha")
-    caixaNome.setAttribute("class", "caixa1")
-    caixaIdade.setAttribute("class", "caixa2")
-    caixaNota.setAttribute("class", "caixa3")
 
   
 }
